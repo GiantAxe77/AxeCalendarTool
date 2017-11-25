01b64ea7db389d02ea2e61f6d4f67ded4eb25d4f
GiantAxe <giantaxe77@163.com>
2017-11-25 23:46:12 +0800

    限制窗口大小，新增mainWindow controller

---
+++ AxeCalendarTool/AxeCalendarTool/MainWindow/AxeMainWindowController.h
@@ -0,0 +1,15 @@
+//
+//  AxeMainWindowController.h
+//  AxeCalendarTool
+//
+//  Created by GiantAxe on 17/11/25.
+//  Copyright © 2017年 GiantAxe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AxeMainWindowController : NSWindowController
+
+@property (weak) IBOutlet NSWindow *window;
+
+@end
